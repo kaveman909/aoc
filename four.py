5143e96448a1e913b7f121975b99c1cba0d1e6df
David <david.tracy3@gmail.com>
2017-12-04 00:14:18 -0500

    day 4

---
+++ four.py
@@ -0,0 +1,20 @@
+import csv
+
+with open('four.txt', 'r') as f:
+    reader = csv.reader(f, delimiter=' ')
+    lines = list(reader)
+    for part in [1,2]:
+        good = 0
+        for line in lines:
+            bad = 0
+            for i in range(len(line)):
+                for j in range(i+1, len(line)):
+                    if part == 1:
+                        if line[i] == line[j]:
+                            bad = 1
+                    if part == 2:
+                        if ''.join(sorted(line[i])) == ''.join(sorted(line[j])):
+                            bad = 1
+            if bad == 0:
+                good += 1
+        print(good)
