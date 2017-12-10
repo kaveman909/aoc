4317b3b279930c639bf35e1b5b794bdc8e8f5f52
David <david.tracy3@gmail.com>
2017-12-10 01:34:38 -0500

    formatting

--- ten.py
+++ ten.py
@@ -1,13 +1,16 @@
 from collections import deque
 
+
 def rotate(lst, x):
     d = deque(lst)
     d.rotate(x)
     lst[:] = d
 
-def reverse(lst,start,end):
+
+def reverse(lst, start, end):
     lst[start:end] = lst[start:end][::-1]
     return lst
+
 
 length_seq = '183,0,31,146,254,240,223,150,2,206,161,1,255,232,199,88'
 length_seq = [ord(c) for c in length_seq]
@@ -26,9 +29,9 @@ rotate(nums, rotation_count)
 
 dense_hash = []
 for i in range(16):
-    xor = nums[i*16]
+    xor = nums[i * 16]
     for j in range(1, 16):
-        xor = xor ^ nums[i*16 + j]
+        xor = xor ^ nums[i * 16 + j]
     dense_hash.append(xor)
 
 hex_hash = ""
