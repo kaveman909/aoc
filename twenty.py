9db05ccdb833fd4038675b58720b7e0db33a9620
David <david.tracy3@gmail.com>
2018-01-10 22:43:11 -0500

    day 20 part 2

--- twenty.py
+++ twenty.py
@@ -10,7 +10,7 @@ for line in lines:
     for i in range(len(openc)):
         lst.append([int(j) for j in (line[openc[i] + 1:closec[i]]).split(',')])
     mlist.append(lst)
-
+#print(mlist, len(mlist))
 # Find minimum acceleration
 min_val = 100000
 ind = 0
@@ -23,7 +23,29 @@ for line in mlist:
     ind += 1
 print('Part 1: {}'.format(min_ind))
 
-# nlist = []
-# for line in mlist:
+for k in range(50):
     # check for duplicates
-        
+    for i in range(len(mlist)):
+        dup = False
+        dup_list = []
+        for j in range(len(mlist)):
+            try:
+                if (mlist[i][0] == mlist[j][0]) and (i != j):
+                    dup = True
+                    dup_list.append(mlist[j])
+            except:
+                pass
+        if dup:
+            dup_list.append(mlist[i])
+            mlist = [l for l in mlist if l not in dup_list]
+
+    # update position and velocity
+    for vector in mlist:
+        p = vector[0]
+        v = vector[1]
+        a = vector[2]
+        v = [i + j for i, j in zip(v, a)]
+        p = [i + j for i, j in zip(p, v)]
+        vector[0] = p
+        vector[1] = v
+print('Part 2: {}'.format(len(mlist)))
